18874c57bfb5c64c7964b4f6ec0a4f41a30616fa
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-10-30 14:34:09 +0200

    feat: add messages prop to StarRating and fix import paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import './index.css'
-// import App from './App.jsx'
+// import "./index.css";
+// import App from "./App.jsx";
 
 import StarRating from "./components/StarRating";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
   </StrictMode>
 );
